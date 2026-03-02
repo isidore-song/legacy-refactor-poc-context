ab24d2c21a994d91d2f5a4c06c89c0c1474243fd
Isidore Song <isidore.sgr.dev@gmail.com>
2026-03-02 15:15:51 +0900

    introduced GrantCouponContext

--- src/main/kotlin/io/github/isidoresong/legacyrefactorpoccontext/point/adapter/LegacyPointAdapter.kt
+++ src/main/kotlin/io/github/isidoresong/legacyrefactorpoccontext/point/adapter/LegacyPointAdapter.kt
@@ -2,6 +2,8 @@ package io.github.isidoresong.legacyrefactorpoccontext.point.adapter
 
 import io.github.isidoresong.legacyrefactorpoccontext.point.port.PointPort
 import io.github.isidoresong.legacyrefactorpoccontext.point.service.PointService
+import io.github.isidoresong.legacyrefactorpoccontext.purchase.model.PurchaseHistory
+import io.github.isidoresong.legacyrefactorpoccontext.user.model.User
 import org.springframework.stereotype.Component
 
 @Component
@@ -11,6 +13,10 @@ class LegacyPointAdapter(
 
     override fun grantByPolicy(userId: String, policyCode: String): Long? {
         return pointService.grantPointByPolicy(userId, policyCode)
+    }
+
+    override fun grantByPolicy(user: User, purchaseHistory: PurchaseHistory?, policyCode: String): Long? {
+        return pointService.grantPointByPolicy(user, purchaseHistory, policyCode)
     }
 
     override fun revokeByPolicy(userId: String, policyCode: String) {
