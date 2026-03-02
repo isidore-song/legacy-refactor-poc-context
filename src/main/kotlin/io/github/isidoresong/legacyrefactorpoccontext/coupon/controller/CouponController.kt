e097ad44162e7992947e810ae288a3c1f66ff9c2
Isidore Song <isidore.sgr.dev@gmail.com>
2026-03-02 14:13:26 +0900

    introduced GrantCouponUseCase

--- src/main/kotlin/io/github/isidoresong/legacyrefactorpoccontext/coupon/controller/CouponController.kt
+++ src/main/kotlin/io/github/isidoresong/legacyrefactorpoccontext/coupon/controller/CouponController.kt
@@ -3,6 +3,7 @@ package io.github.isidoresong.legacyrefactorpoccontext.coupon.controller
 import io.github.isidoresong.legacyrefactorpoccontext.coupon.controller.dto.request.CouponGrantRequest
 import io.github.isidoresong.legacyrefactorpoccontext.coupon.controller.dto.response.CouponGrantResponse
 import io.github.isidoresong.legacyrefactorpoccontext.coupon.service.CouponService
+import io.github.isidoresong.legacyrefactorpoccontext.coupon.usecase.GrantCouponUseCase
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/v1/coupons")
 class CouponController (
-    private val couponService: CouponService
+    private val grantCouponUseCase: GrantCouponUseCase,
 ) {
 
     @PostMapping
     fun grantCoupon(@RequestBody request: CouponGrantRequest) : ResponseEntity<CouponGrantResponse> {
-        val couponResult = couponService.grantCoupon(request.userId, request.couponCode)
+        val couponResult = grantCouponUseCase.execute(request.userId, request.couponCode)
         if(!couponResult.success) {
             return ResponseEntity.ok(
                 CouponGrantResponse(
