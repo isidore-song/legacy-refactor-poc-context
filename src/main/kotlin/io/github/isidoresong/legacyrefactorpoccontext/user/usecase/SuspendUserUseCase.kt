c1647c684008b865ed325e0f6a069fdba91cd986
Isidore Song <isidore.sgr.dev@gmail.com>
2026-03-02 15:36:33 +0900

    introduced SuspendUserContext

--- src/main/kotlin/io/github/isidoresong/legacyrefactorpoccontext/user/usecase/SuspendUserUseCase.kt
+++ src/main/kotlin/io/github/isidoresong/legacyrefactorpoccontext/user/usecase/SuspendUserUseCase.kt
@@ -1,13 +1,12 @@
 package io.github.isidoresong.legacyrefactorpoccontext.user.usecase
 
-import io.github.isidoresong.legacyrefactorpoccontext.common.exception.UserNotFoundException
 import io.github.isidoresong.legacyrefactorpoccontext.coupon.port.CouponPort
 import io.github.isidoresong.legacyrefactorpoccontext.point.port.PointPort
 import io.github.isidoresong.legacyrefactorpoccontext.user.event.UserSuspendedEvent
-import io.github.isidoresong.legacyrefactorpoccontext.user.model.ActionType
 import io.github.isidoresong.legacyrefactorpoccontext.user.model.Status
 import io.github.isidoresong.legacyrefactorpoccontext.user.model.User
 import io.github.isidoresong.legacyrefactorpoccontext.user.repository.UserRepository
+import io.github.isidoresong.legacyrefactorpoccontext.user.usecase.context.SuspendUserContext
 import io.github.isidoresong.legacyrefactorpoccontext.userAction.service.UserActionLogService
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
@@ -21,23 +20,19 @@ class SuspendUserUseCase(
     private val eventPublisher: ApplicationEventPublisher,
 ) {
     fun execute(userId: String): User {
-        val user = userRepository.findById(userId)
-            ?: throw UserNotFoundException("User with id '$userId' not found.")
+        val ctx = SuspendUserContext(userId, userRepository, userActionLogService)
 
+        val user = ctx.user
         if (user.status == Status.QUITTER) throw IllegalStateException("User with id '$userId' is already a quitter.")
         if (user.status == Status.SUSPENDED) throw IllegalStateException("User with id '$userId' is already suspended.")
 
         val savedUser = userRepository.save(User(id = user.id, name = user.name, region = user.region, gender = user.gender, status = Status.SUSPENDED))
 
-        val lastCouponLogs = userActionLogService.getLastActionLogs(ActionType.COUPON_GRANT, userId, 7)
-        val lastPointLogs = userActionLogService.getLastActionLogs(ActionType.POINT_GRANT, userId, 7)
-
-        lastCouponLogs.forEach { log ->
-            couponPort.revokeByCode(log.userId, log.detail)
+        ctx.lastCouponGrantLogs.forEach {
+            couponPort.revokeByCode(it.userId, it.detail)
         }
-
-        lastPointLogs.forEach { log ->
-            pointPort.revokeByPolicy(log.userId, log.detail)
+        ctx.lastPointGrantLogs.forEach {
+            pointPort.revokeByPolicy(it.userId, it.detail)
         }
 
         eventPublisher.publishEvent(UserSuspendedEvent(userId))
